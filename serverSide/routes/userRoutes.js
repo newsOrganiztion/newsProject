d25843c42071eb9323cb713103e9912bc1ab2760
Hareth2000 <harethj18@gmail.com>
2025-03-13 14:37:14 +0300

    done save token in cookies

--- serverSide/routes/userRoutes.js
+++ serverSide/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser ,googleLogin  } = require('../controllers/userController');
+const { registerUser, loginUser ,googleLogin, getUserProfile } = require('../controllers/userController');
+const verifyToken = require('../Middlewares/authMiddleware');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/google-login', googleLogin);
-
+router.get('/profile', verifyToken, getUserProfile);  // إضافة الميدلوير للتحقق من التوكن
 
 module.exports = router;
